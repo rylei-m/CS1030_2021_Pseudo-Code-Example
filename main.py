a76d8830b81d4d1e9aff95bf266ca5bdf6f6cd36
rylei-m <149725191+rylei-m@users.noreply.github.com>
2024-02-07 19:36:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,52 @@
+#begin
+  #numeric nRadius, nHeight
+  #display "Input the radius."
+  #accept nRadius
+  #display "Input the height."
+  #accept nHeight
+  #nArea = 2*π*nRadius*nHeight+2*π*nRadius*2
+  #display "Area of Cylinder: " nArea
+#end
+
+#int = whole numbers
+#float = decimal numbers
+#bool = true or False
+
+my_string = '...'
+print(my_string)
+
+num_1 = 1
+print(num_1)
+num_2 = 1
+print(num_2)
+num_3 = 7
+print(num_3)
+
+if num_1 > num_2:
+  print("..")
+else:
+  print(".")
+if num_1 == num_2:
+  print(",")
+else:
+  print(",,")
+
+if num_2 > num_3:
+  print("...")
+else:
+  print("....")
+if num_2 == num_3:
+  print(",")
+else:
+  print(",,")
+
+if num_1 > num_3:
+  print(".....")
+else:
+  print("......")
+if num_1 == num_3:
+  print(",")
+else:
+  print(",,")
+
+
